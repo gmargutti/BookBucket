85f4d565e051fee6300cff7f32c46bb3ed3e17a1
Gabriel Beltrame Silva <gbs_yrp@hotmail.com>
2013-02-24 14:44:44 -0300

    Removed old dependencies, added menu class
    Classe login agora é um JFrame ao invés de WindowApplication para
    remover a dependência do forms.jar

--- BookBucket/src/principal/Login.java
+++ BookBucket/src/principal/Login.java
@@ -3,20 +3,25 @@ package principal;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.Toolkit;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-import javax.swing.SwingConstants;
+import java.awt.Toolkit;
 
-public class Login {
-
-	private JFrame frmLogin;
-	private JPasswordField txtsenha;
+public class Login extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3817989945944490744L;
+	private JPanel contentPane;
 	private JTextField txtlogin;
+	private JPasswordField txtsenha;
 
 	/**
 	 * Launch the application.
@@ -25,8 +30,8 @@ public class Login {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Login window = new Login();
-					window.frmLogin.setVisible(true);
+					Login frame = new Login();
+					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,54 +40,44 @@ public class Login {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public Login() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frmLogin = new JFrame();
-		frmLogin.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		frmLogin.setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/images/BookBucket.png")));
-		frmLogin.setResizable(false);
-		frmLogin.setTitle("Login");
-		frmLogin.setBounds(100, 100, 350, 150);
-		frmLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmLogin.getContentPane().setLayout(null);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/images/BookBucket.png")));
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 150);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
 		JLabel lblLogin = new JLabel("Login:");
 		lblLogin.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblLogin.setBounds(10, 11, 72, 22);
-		frmLogin.getContentPane().add(lblLogin);
+		lblLogin.setBounds(10, 11, 54, 22);
+		contentPane.add(lblLogin);
 		
 		JLabel lblSenha = new JLabel("Senha:");
 		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblSenha.setBounds(10, 36, 72, 22);
-		frmLogin.getContentPane().add(lblSenha);
-		
-		txtsenha = new JPasswordField();
-		txtsenha.setBounds(67, 40, 141, 20);
-		frmLogin.getContentPane().add(txtsenha);
+		lblSenha.setBounds(10, 33, 54, 22);
+		contentPane.add(lblSenha);
 		
 		txtlogin = new JTextField();
-		txtlogin.setBounds(67, 13, 141, 20);
-		frmLogin.getContentPane().add(txtlogin);
+		txtlogin.setBounds(68, 13, 126, 20);
+		contentPane.add(txtlogin);
 		txtlogin.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnNewButton.setIcon(new ImageIcon(Login.class.getResource("/images/BookBucket40x40.png")));
-		btnNewButton.setBounds(218, 11, 116, 49);
-		frmLogin.getContentPane().add(btnNewButton);
+		txtsenha = new JPasswordField();
+		txtsenha.setBounds(68, 37, 126, 20);
+		contentPane.add(txtsenha);
 		
-		JLabel lblNewLabel = new JLabel("Ver 1.0 Beta");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel.setBounds(253, 97, 81, 14);
-		frmLogin.getContentPane().add(lblNewLabel);
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setIcon(new ImageIcon(Login.class.getResource("/images/BookBucket40x40.png")));
+		btnNewButton.setBounds(204, 11, 120, 44);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Ver 1.0 beta");
+		lblNewLabel.setBounds(258, 87, 66, 14);
+		contentPane.add(lblNewLabel);
 	}
 }
---
+++ BookBucket/src/principal/Menu.java
@@ -0,0 +1,48 @@
+package principal;
+
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+
+public class Menu extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9135654319942094252L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menu() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Menu.class.getResource("/images/BookBucket.png")));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
